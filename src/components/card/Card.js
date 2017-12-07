95e03fac89217a0ec0feb999f34cfd2ebe1f6ad1
Neil <neilspurgeon@gmail.com>
2017-12-07 00:07:04 -0800

    styled cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,7 +5,10 @@ class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      latestPrice: null
+      latestPrice: null,
+      buyModalIsOpen: false,
+      sellModalIsOpen: false,
+      transationAmount: 100
     };
   }
 
@@ -28,12 +31,48 @@ class Card extends React.Component {
       });
   }
 
+  // Handle open/close modals
+
+  handleOpenBuyModal = () => {
+    console.log('open');
+    this.setState({
+      buyModalIsOpen: true
+    });
+  }
+
+  handleCloseBuyModal = () => {
+    this.setState({
+      buyModalIsOpen: false
+    });
+  }
+
+  handleOpenSellModal = () => {
+    console.log('open');
+    this.setState({
+      sellModalIsOpen: true
+    });
+  }
+
+  handleCloseSellModal = () => {
+    this.setState({
+      sellModalIsOpen: false
+    });
+  }
+
+  handleTransactionAmount = (e) => {
+    this.setState({
+      transationAmount: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="card">
 
-        <h2 className="card-title">{this.props.symbol}</h2>
-        <span className="price">{this.state.latestPrice}</span>
+        <div className="card-header">
+          <h2 className="card-title">{this.props.symbol}</h2>
+          <span className="price">{this.state.latestPrice}</span>
+        </div>
 
         <div className="data-container">
 
@@ -56,6 +95,60 @@ class Card extends React.Component {
             <span className="data-label">Shares</span>
             <span className="data-value">{this.props.shares}</span>
           </div>
+
+        </div>
+
+        <div className="bottom-actions">
+
+          <button
+            className="purchase-btn card-btn"
+            onClick={this.handleOpenBuyModal}
+          >
+            BUY
+          </button>
+
+          {this.props.isSellable ?
+            <button
+              className="sell-btn card-btn"
+              onClick={this.handleOpenSellModal}
+            >
+              SELL
+            </button>
+          : null}
+
+        </div>
+
+        <div className={["buy-modal", "card-modal", (this.state.buyModalIsOpen ? "is-open" : null)].join(' ')}>
+
+          How much would you like to purchase?
+
+          <input
+            className="transaction-input"
+            type="number"
+            value={this.state.transationAmount}
+            onChange={this.handleTransactionAmount}
+          />
+
+          <button className="confirm-btn card-btn">
+            Confirm Purchase
+          </button>
+
+        </div>
+
+        <div className={["sell-modal", "card-modal", (this.state.sellModalIsOpen ? "is-open" : null)].join(' ')}>
+
+          How much would you like to sell?
+
+          <input
+            className="transaction-input"
+            type="number"
+            value={this.state.transationAmount}
+            onChange={this.handleTransactionAmount}
+          />
+
+        <button className="confirm-btn card-btn">
+          Confirm Sell
+        </button>
 
         </div>
 
